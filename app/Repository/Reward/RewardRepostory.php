23f117b01253922bfa4e6e620788fb1647e6d2df
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-02 13:19:35 +0700

    create repository Reward

---
+++ app/Repository/Reward/RewardRepostory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repository\Reward;
+
+use App\Http\Resources\RewardResource;
+use App\Models\Reward;
+
+class RewardRepostory implements IRewardRepository
+{
+    /**
+     * @param Reward $reward
+     */
+    public function __construct(Reward $reward)
+    {
+        $this->reward = $reward;
+    }
+
+
+    public function list(): object
+    {
+        return RewardResource::collection($this->reward->newQuery()->get());
+    }
+
+    public function create($data)
+    {
+        return $this->reward
+                    ->newQuery()
+                    ->create($data);
+    }
+
+    public function update($data, int $rewardId)
+    {
+        return $this->reward
+                    ->newQuery()
+                    ->findOrFail($rewardId)
+                    ->update($data);
+    }
+
+    public function destroy(int $rewardId)
+    {
+        return $this->reward
+            ->newQuery()
+            ->findOrFail($rewardId)
+            ->delete();
+    }
+}
