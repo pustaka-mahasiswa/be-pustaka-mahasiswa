2ac17234df3986ebdd469043cd050490c1bef554
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-02 23:05:30 +0700

    interface repository userReward and findRewardById

--- app/Repository/Reward/IRewardRepository.php
+++ app/Repository/Reward/IRewardRepository.php
@@ -6,9 +6,13 @@ interface IRewardRepository
 {
     public function list(): object;
 
+    public function findRewardById(int $rewardId): object;
+
     public function create($data): void;
 
     public function update($data, int $rewardId): void;
 
     public function destroy(int $rewardId): void;
+
+    public function userReward(object $reward, object $user): void;
 }
