499d3b70ebbf6837224ab50ecf8271ccdbe87b7b
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-02 12:51:35 +0700

    add total pocket money and change name to username

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('total_pocket_money')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
