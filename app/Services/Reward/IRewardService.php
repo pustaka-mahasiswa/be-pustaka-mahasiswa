8c0585d0b4774cb66efa9c4d91a5ab5b88d562cf
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-02 14:23:12 +0700

    clean code

--- app/Services/Reward/IRewardService.php
+++ app/Services/Reward/IRewardService.php
@@ -2,15 +2,16 @@
 
 namespace App\Services\Reward;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Request;
 
 interface IRewardService
 {
     public function listReward(Request $request): object;
 
-    public function createReward(Request $request): void;
+    public function createReward(Request $request): JsonResponse;
 
-    public function updateReward(Request $request, int $rewardId): void;
+    public function updateReward(Request $request, int $rewardId): JsonResponse;
 
-    public function deleteReward(int $rewardId): void;
+    public function deleteReward(int $rewardId): JsonResponse;
 }
