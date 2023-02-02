5c5ddbe486d6287661d76738b9bf9d63862d0bcd
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-02 12:28:16 +0700

    login resource

---
+++ app/Http/Resources/LoginResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LoginResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            "amount_coin" => $this->amount_coin,
+            "token" => $this->createToken('token')->plainTextToken
+        ];
+    }
+}
