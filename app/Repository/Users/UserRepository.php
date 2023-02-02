06f0b1a06d67039114c71acbd4fdb7d1cd923c7a
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-02 12:29:05 +0700

    create implement intreface repository user

---
+++ app/Repository/Users/UserRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repository\Users;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserRepository implements IUserRepository
+{
+    private User $user;
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function findByUsername(string $username): object
+    {
+        return $this->user->where('username', $username)->first();
+    }
+
+    public function create(array $data): bool
+    {
+        $data['password'] = Hash::make($data['password']);
+        $this->user->create($data);
+        return true;
+    }
+
+
+}
