db94a81fde2082a9c075b32b2180cbf9ebe1adc0
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-02 14:22:30 +0700

    add 2 methode and implement logic in controller reward
    - method index
    - method create

--- app/Http/Controllers/RewardController.php
+++ app/Http/Controllers/RewardController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\Reward\IRewardService;
+use Illuminate\Support\Facades\Request;
 
 class RewardController extends Controller
 {
+    /**
+     * @param IRewardService $rewardService
+     */
+    public function __construct(IRewardService $rewardService)
+    {
+        $this->rewardService = $rewardService;
+    }
 
+
+    public function index()
+    {
+        $listReward = $this->rewardService->listReward();
+        return response()->json($listReward, 200);
+    }
+
+    public function create(Request $request)
+    {
+        return $this->rewardService->createReward($request->all());
+    }
 }
