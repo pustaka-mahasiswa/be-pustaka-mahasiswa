7a7b8325cbf87a2c71b27f580e58b510731df932
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-02 12:51:11 +0700

    implement lazzy loding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        JsonResource::withoutWrapping();
+
+        Model::preventLazyLoading(! app()->isProduction());
     }
 }
