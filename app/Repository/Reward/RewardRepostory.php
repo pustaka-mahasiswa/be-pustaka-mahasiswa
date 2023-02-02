721dfaa8dc31b71ba040b35e812cfc6046990261
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-02 23:05:14 +0700

    logic save user reward

--- app/Repository/Reward/RewardRepostory.php
+++ app/Repository/Reward/RewardRepostory.php
@@ -3,22 +3,38 @@
 namespace App\Repository\Reward;
 
 use App\Http\Resources\RewardResource;
+use App\Models\LoginReward;
 use App\Models\Reward;
+use Illuminate\Support\Facades\DB;
 
 class RewardRepostory implements IRewardRepository
 {
+    private Reward $reward;
+    private LoginReward $loginReward;
+
     /**
      * @param  Reward  $reward
      */
-    public function __construct(Reward $reward)
+    public function __construct(Reward $reward, LoginReward $loginReward)
     {
         $this->reward = $reward;
+        $this->loginReward = $loginReward;
     }
 
     public function list(): object
     {
         return RewardResource::collection($this->reward->newQuery()->get());
     }
+
+    public function findRewardById(int $rewardId): object
+    {
+        return new RewardResource(
+            $this->reward
+                ->newQuery()
+                ->findOrFail($rewardId)
+        );
+    }
+
 
     public function create($data): void
     {
@@ -42,4 +58,46 @@ class RewardRepostory implements IRewardRepository
             ->findOrFail($rewardId)
             ->delete();
     }
+
+    private function doubleBonusSaku($reward): int
+    {
+        $startOfWeek = Carbon::now()->startOfWeek();
+        $endOfWeek = Carbon::now()->endOfWeek();
+
+         $weeklyTotal = $this->loginReward
+             ->newQuery()
+             ->whereBetween('created_at', [$startOfWeek, $endOfWeek])
+             ->sum();
+
+         if ($weeklyTotal % 3 == 0)
+         {
+             $reward *= 2;
+         }
+         return $reward;
+    }
+
+    public function userReward(object $reward, object $user): void
+    {
+
+        try {
+            DB::beginTransaction();
+            $this->loginReward
+                ->newQuery()
+                ->create([
+                    "reward_id" => $reward->id,
+                    'user_id' => $user->id
+                ]);
+            $rewardValue =$this->doubleBonusSaku($reward->value);
+            $user->total_pocket_money += $rewardValue;
+            $user->save();
+            DB::commit();
+        } catch (\Exception $e)
+        {
+            report($e);
+            DB::rollBack();
+        }
+
+    }
+
+
 }
