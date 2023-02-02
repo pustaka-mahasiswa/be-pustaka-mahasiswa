95a0ee4c39ef1f565dea4ea97c8b210cb2efd589
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2023-02-02 13:19:28 +0700

    create interface repository Reward

---
+++ app/Repository/Reward/IRewardRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repository\Reward;
+
+interface IRewardRepository
+{
+    public function list() : object;
+
+    public function create($data);
+
+    public function update($data, int $rewardId);
+
+    public function destroy(int $rewardId);
+}
